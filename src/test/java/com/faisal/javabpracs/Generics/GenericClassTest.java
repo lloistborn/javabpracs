b0a8ba480ddb5a368c76f837640d98cc5e88c8ad
lloisborn <faisalmorensya@gmail.com>
2017-12-22 14:50:21 +0700

    test generic type class

--- src/test/java/com/faisal/javabpracs/Generics/GenericClassTest.java
+++ src/test/java/com/faisal/javabpracs/Generics/GenericClassTest.java
@@ -8,5 +8,15 @@ import static org.junit.Assert.*;
  * Created by faisalmorensya on 22/12/17.
  */
 public class GenericClassTest {
-    
+
+    @Test
+    public void testUsingRawType() {
+        GenericClass type = new GenericClass();
+
+        type.setT("Hello");
+        assertEquals("Hello", type.getT());
+
+        type.setT(10);
+        assertEquals(10, type.getT());
+    }
 }
