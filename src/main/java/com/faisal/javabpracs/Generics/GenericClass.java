006e77d3a3da10934ff017c06d1db15160a2504a
lloisborn <faisalmorensya@gmail.com>
2017-12-22 14:13:51 +0700

    init generic class

---
+++ src/main/java/com/faisal/javabpracs/Generics/GenericClass.java
@@ -0,0 +1,16 @@
+package com.faisal.javabpracs.Generics;
+
+/**
+ * Created by faisalmorensya on 22/12/17.
+ */
+public class GenericClass<T> {
+    private T t;
+
+    public T getT() {
+        return t;
+    }
+
+    public void setT(T t) {
+        this.t = t;
+    }
+}
---
+++ src/test/java/com/faisal/javabpracs/Generics/GenericClassTest.java
@@ -0,0 +1,12 @@
+package com.faisal.javabpracs.Generics;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by faisalmorensya on 22/12/17.
+ */
+public class GenericClassTest {
+    
+}
